825d18785f2fa134a20c3e874d4c3974d1322230
Nitin John <nitinjohnraj@gmail.com>
2016-07-04 16:30:38 +0530

    Created admin for quiz_bot.

--- quizbot/admin.py
+++ quizbot/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 from quizbot.models import *
 
 
-models
+models = [BotUser, Subject, Topic, Question, Option, Quiz, AttemptedPaper, Answer, StudentQuizReview]
+
+for model in models:
+    admin.site.register(model)
